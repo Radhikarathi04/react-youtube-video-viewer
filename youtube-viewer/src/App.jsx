b2d499ae02123317c296239073b03f7ceb27457b
Radhika <radhikarathi308@gmail.com>
2025-05-30 17:17:02 +0100

    completed app

--- youtube-viewer/src/App.jsx
+++ youtube-viewer/src/App.jsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useState , useEffect} from 'react';
+import { Video } from './Video';
+import { Search } from './Search';
 
 const App = () => {
+  const baseUrl = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.youtube.com%2Ffeeds%2Fvideos.xml%3Fchannel_id%3D'
+  const [videos, setVideos] = useState([]);
+  const [currentChannelId, setCurrentChannelId] = useState();
+  const [channelName, setChannelName] = useState('');
+  const [searchError, setSearchError] = useState('');
+
+  // This code will rerun every time the currentChannelId changes
+  useEffect(() => {
+    (async () => {
+      if (currentChannelId) {
+          try {
+            const data = await fetch(`${baseUrl}${currentChannelId}`)
+              .then(response => response.json());
+            if(!data.items) {
+              throw new Error();
+            }
+            // how do we know that the data returned will have items?
+            setVideos(data.items);
+            setChannelName(data.items[0].author);
+            setSearchError('');
+            console.log(data.items);  
+          } catch (error) {
+            console.log(error);
+            setSearchError('Could not retrieve videos. Please check the channel ID and try again.');
+            
+          }
+        }
+      })();
+  }, [currentChannelId]);
+  
   return (
-    <div>
+    <div className='app-container'>
       <h1>Latest YouTube Videos</h1>
+      <Search setCurrentChannelId={id => setCurrentChannelId(id)}/>
+      <div className="search__errors">
+        {searchError}
+      </div>
+      {channelName &&  <h2>Videos from {channelName}</h2>}
+      <div className="videos">
+        {videos.map(video => <Video key={video.guid} video={video} />)}
+      </div>
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ youtube-viewer/src/Search.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+export const Search = ({ setCurrentChannelId }) => {
+    const [channelId, setChannelId] = useState('');
+
+    return (
+        <div className="search">
+        <input type="text" onChange={event => setChannelId(event.target.value)}placeholder='Enter your favourite channel ID'/>
+        <button onClick={() => setCurrentChannelId(channelId)}>Get Videos</button>
+      </div>
+    )
+};
